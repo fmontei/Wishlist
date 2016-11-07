ce30508bf58de1f8c02a3fa7987c54f56c57f2ed
Julian Sy <js802v@att.com>
2016-11-07 08:54:14 -0500

    be able to pull wish score

--- routes/get_wish.js
+++ routes/get_wish.js
@@ -7,23 +7,38 @@ var db = new sqlite3.Database('wishlist.db');
 
 
 router.use(function(req, res, next) {
+    var wishes = []
    	async.waterfall([
    		function get_user(callback) {
    			db.all('select * from wish;', {
 		    }, function(err, rows) {
 		    	if (rows) {
-				console.log("THERE WERE ROWS")
-				console.log(rows)
-				wish = rows;
-			}
+				    //console.log("THERE WERE WISHES")
+				    //console.log(rows)
+				    wishes = rows;
+			    }
 		    	callback(err);
 		    });
    		},
+        function get_vote_count(callback) {
+            for(i in wishes) {
+                var wish = wishes[i];
+                db.all("select * from vote where wish_id=" + wish.wish_id, {
+                }, function(err, rows) {
+                    if (rows) {
+                        //console.log("THERE WERE VOTES");
+                        //console.log(rows)
+                        wish.score = rows.length;
+                    }
+                    callback(err);
+                });
+            }
+        }
    	], function(err) {
    		if (err) {
    			return res.status(500).send(err);
    		} else {
-   			return res.status(200).send(wish);
+   			return res.status(200).send(wishes);
    		}
    	});
 });
